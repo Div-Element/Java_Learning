ad16010f03f20f7279c4797aa47f6341819b19dc
Timethor <Nick.o.rich@gmail.com>
2013-07-19 22:05:16 -0400

    Created a new organization system to that adding new assignments can be easily ordered and tracked

---
+++ src/com/timethor/teaching/java/assignments/A01/HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.timethor.teaching.java.assignments.A01.HelloWorld;
+
+/**
+ *
+ * @author Timethor
+ */
+public class HelloWorld {
+
+    /**
+     *
+     * Follow the Homework_Specification.pdf file to complete the
+     * assignment
+     * <p/>
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
