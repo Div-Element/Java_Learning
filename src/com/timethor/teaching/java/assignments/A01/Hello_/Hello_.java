dac6cc2bc50d6c18026b28727c51d378d706c4ff
Timethor <Nick.o.rich@gmail.com>
2013-07-23 23:43:17 -0400

    Addition of "Say Hi to the user!" Assignment TODO: Bonus section parts and potentially more content for the main assignment

---
+++ src/com/timethor/teaching/java/assignments/A01/Hello_/Hello_.java
@@ -0,0 +1,18 @@
+package com.timethor.teaching.java.assignments.A01.Hello_;
+
+/**
+ *
+ * @author Timethor
+ */
+public class Hello_ {
+
+    /**
+     *
+     * Follow the Homework_Specification.pdf file to complete the
+     * assignment
+     * <p/>
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
